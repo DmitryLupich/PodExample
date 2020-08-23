25705ae17de165d79089069b877ac39f97420ddc
Dmitriy Lupych <dmitriy.lupych@wikrgroup.com>
2020-08-23 21:32:33 +0300

    Initial Commit

---
+++ CombineUIKit/CombineUIKit.h
@@ -0,0 +1,19 @@
+//
+//  CombineUIKit.h
+//  CombineUIKit
+//
+//  Created by Dmitriy Lupych on 23.08.2020.
+//  Copyright © 2020 Dmitriy Lupych. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CombineUIKit.
+FOUNDATION_EXPORT double CombineUIKitVersionNumber;
+
+//! Project version string for CombineUIKit.
+FOUNDATION_EXPORT const unsigned char CombineUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CombineUIKit/PublicHeader.h>
+
+
